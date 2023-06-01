3e20937e001770c4df9ce8487e867dcb10b44e7e
Юра Гуцалюк <hutsaluck@gmail.com>
2023-06-01 07:33:44 +0300

    Create first structure

---
+++ resources/js/app.js
@@ -0,0 +1,10 @@
+import './bootstrap';
+
+import Vue from 'vue'
+
+Vue.component('posts-index',
+    require('./components/Posts/Index.vue').default)
+
+const app = new Vue({
+    el: '#app'
+})
---
+++ resources/js/components/Posts/Index.vue
@@ -0,0 +1,37 @@
+<template>
+    <table class="table">
+        <thead>
+        <tr>
+            <th>Title</th>
+            <th>Post text</th>
+            <th>Created date</th>
+            <th>Actions </th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>Something 1</td>
+            <td>Longer text</td>
+            <td>2023-06-01</td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>Something 2</td>
+            <td>Longer text</td>
+            <td>2023-06-01</td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>Something 3</td>
+            <td>Longer text</td>
+            <td>2023-06-01</td>
+            <td></td>
+        </tr>
+        </tbody>
+    </table>
+</template>
+
+
+<script>
+
+</script>
