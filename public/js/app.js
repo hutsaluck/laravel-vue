cdb4b7e8f79af7228f5496103dba7030363a696c
Юра Гуцалюк <hutsaluck@gmail.com>
2023-06-05 09:15:06 +0300

    Create form of create posts

--- public/js/app.js
+++ public/js/app.js
@@ -1908,7 +1908,32 @@ module.exports = {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony default export */ __webpack_exports__["default"] = ({});
+/* harmony default export */ __webpack_exports__["default"] = ({
+  data: function data() {
+    return {
+      categories: {},
+      fields: {
+        title: '',
+        post_text: '',
+        category_id: ''
+      }
+    };
+  },
+  mounted: function mounted() {
+    var _this = this;
+    axios.get('api/categories').then(function (response) {
+      _this.categories = response.data.data;
+    });
+  },
+  methods: {
+    submit_form: function submit_form() {
+      var _this2 = this;
+      axios.post('api/posts', this.fields).then(function (response) {
+        _this2.$router.push('/');
+      });
+    }
+  }
+});
 
 /***/ }),
 
@@ -2059,7 +2084,85 @@ __webpack_require__.r(__webpack_exports__);
 var render = function render() {
   var _vm = this,
     _c = _vm._self._c;
-  return _c("div", [_vm._v("\n    We will have a create form here.\n")]);
+  return _c("div", [_c("form", {
+    on: {
+      submit: function submit($event) {
+        $event.preventDefault();
+        return _vm.submit_form.apply(null, arguments);
+      }
+    }
+  }, [_vm._v("\n        Post title:\n        "), _c("br"), _vm._v(" "), _c("input", {
+    directives: [{
+      name: "model",
+      rawName: "v-model",
+      value: _vm.fields.title,
+      expression: "fields.title"
+    }],
+    staticClass: "form-control",
+    attrs: {
+      type: "text"
+    },
+    domProps: {
+      value: _vm.fields.title
+    },
+    on: {
+      input: function input($event) {
+        if ($event.target.composing) return;
+        _vm.$set(_vm.fields, "title", $event.target.value);
+      }
+    }
+  }), _vm._v(" "), _c("br"), _vm._v("\n        Post text:\n        "), _c("br"), _vm._v(" "), _c("textarea", {
+    directives: [{
+      name: "model",
+      rawName: "v-model",
+      value: _vm.fields.post_text,
+      expression: "fields.post_text"
+    }],
+    staticClass: "form-control",
+    attrs: {
+      rows: "10"
+    },
+    domProps: {
+      value: _vm.fields.post_text
+    },
+    on: {
+      input: function input($event) {
+        if ($event.target.composing) return;
+        _vm.$set(_vm.fields, "post_text", $event.target.value);
+      }
+    }
+  }), _vm._v(" "), _c("br"), _vm._v("\n        Category:\n        "), _c("select", {
+    directives: [{
+      name: "model",
+      rawName: "v-model",
+      value: _vm.fields.category_id,
+      expression: "fields.category_id"
+    }],
+    staticClass: "form-control",
+    on: {
+      change: function change($event) {
+        var $$selectedVal = Array.prototype.filter.call($event.target.options, function (o) {
+          return o.selected;
+        }).map(function (o) {
+          var val = "_value" in o ? o._value : o.value;
+          return val;
+        });
+        _vm.$set(_vm.fields, "category_id", $event.target.multiple ? $$selectedVal : $$selectedVal[0]);
+      }
+    }
+  }, _vm._l(_vm.categories, function (category) {
+    return _c("option", {
+      domProps: {
+        value: category.id
+      }
+    }, [_vm._v(_vm._s(category.name))]);
+  }), 0), _vm._v(" "), _c("br"), _vm._v(" "), _c("input", {
+    staticClass: "btn btn-primary",
+    attrs: {
+      type: "submit",
+      value: "Save post"
+    }
+  })])]);
 };
 var staticRenderFns = [];
 render._withStripped = true;
