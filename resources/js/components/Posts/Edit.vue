f1ef552245bcc6bff4512a76f9834545830114fd
Юра Гуцалюк <hutsaluck@gmail.com>
2023-06-06 09:12:22 +0300

    Create functional for popup information

--- resources/js/components/Posts/Edit.vue
+++ resources/js/components/Posts/Edit.vue
@@ -72,10 +72,12 @@ export default {
 
             axios.put(`/api/posts/${this.$route.params.id}`, this.fields)
                 .then(response => {
+                    this.$swal(`Post updated successfully`)
                     this.$router.push('/')
                     this.form_submitting = false
                 })
                 .catch(error => {
+                    this.$swal({icon: `error`, title: `Error happened`})
                     if(error.response.status === 422){
                         this.errors = error.response.data.errors
                     }
